06d2b91dbdee067c345fe1c0a3750c91bddf8886
Rolando Cruz <rolando.cruz21@gmail.com>
2017-02-09 06:57:13 +0800

    Fix JS errors

--- borrower/includes/scripts.php
+++ borrower/includes/scripts.php
@@ -5,16 +5,12 @@
     <!-- METISMENU SCRIPTS -->
 <script src="assets/js/jquery.metisMenu.js"></script>
 <!-- DATA TABLE SCRIPTS -->
-    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
-    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('#dataTables-example').dataTable();
-            });
-    </script>
-         <!-- CUSTOM SCRIPTS -->
-    <script src="assets/js/custom.js"></script>
-
-      <!-- CUSTOM SCRIPTS -->
+<script src="assets/js/dataTables/jquery.dataTables.js"></script>
+<script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#dataTables-example').dataTable();
+        });
+</script>
+     <!-- CUSTOM SCRIPTS -->
 <script src="assets/js/custom.js"></script>
-
