581179430f8f648d0c693a30e3de7b0ae3d058c5
Rolando Cruz <rolando.cruz21@gmail.com>
2017-02-12 03:24:57 +0800

    Fix logout

--- borrower/ajax/logouts.php
+++ borrower/ajax/logouts.php
@@ -2,8 +2,5 @@
     include 'session.php';
     include 'connection.php';
     session_destroy();
-    $day = date("Y-m-d G:i:s a");
-    $add = "UPDATE log SET logout = '" . date('y-m-d H:i:s') . "'where logID = '$logid'";
-    $resultAdd = $conn->query($add);
     header('Location: ../');
 ?>
