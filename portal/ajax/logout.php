8befc6e94e98c223465fc5aedd5b1ac25dcdbeb7
Rolando Cruz <rolando.cruz21@gmail.com>
2017-02-12 00:10:31 +0800

    Restrict access to most portal pages to librarians only

--- portal/ajax/logout.php
+++ portal/ajax/logout.php
@@ -1,26 +1,31 @@
 <?php
-include 'connection.php';
-        if(!isset($_SESSION['id']) || $_SESSION['id'] == " " || !isset($_SESSION['id']) || $_SESSION['id'] == " ")
-        {
-            echo "<script language='javascript'>";
-            echo "window.location='../'";
-            echo "</script> ";
-            exit;
-        }
+    require_once 'connection.php';
 
-        $_SESSION['start'] = time(); // taking now logged in time
+    if (!isset($_SESSION['id']) || $_SESSION['id'] == " " || !$_SESSION['id']) {
+        echo "<script language='javascript'>";
+        echo "window.location='../'";
+        echo "</script> ";
+        exit;
+    }
 
-        if(!isset($_SESSION['expire'])){
-            $_SESSION['expire'] = $_SESSION['start'] + (60 * 60) ; // ending a session in 30 seconds
-        }
-        $now = time(); // checking the time now when home page starts
+    if ($_SESSION['type'] != "LIBRARIAN") {
+        header('HTTP/1.1 401 Forbidden');
+        echo '<h1>Access Denied</h1>';
+        echo '<a href="javascript:window.history.go(-1)">Go back</a>';
+        exit;
+    }
 
-        if($now > $_SESSION['expire'])
-        {
-            $day = date("Y-m-d G:i:s a");
-            //$add = "UPDATE log SET logout = '" . date('y-m-d H:i:s') . "'where logID = '$logid'";
-            //$resultAdd = $conn->query($add);
-            session_destroy();
-            header('Location: ../');
-        }
+    $_SESSION['start'] = time(); // taking now logged in time
+
+    if (!isset($_SESSION['expire'])) {
+        $_SESSION['expire'] = $_SESSION['start'] + (60 * 60) ; // ending a session in 30 seconds
+    }
+
+    $now = time(); // checking the time now when home page starts
+
+    if ($now > $_SESSION['expire']) {
+        $day = date("Y-m-d G:i:s a");
+        session_destroy();
+        header('Location: ../');
+    }
 ?>
