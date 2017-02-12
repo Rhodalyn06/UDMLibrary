c0377337b11d4c1e069abb67095d4b696e79050c
Rolando Cruz <rolando.cruz21@gmail.com>
2017-02-12 09:48:36 +0800

    Update DB password

--- portal/ajax/connection.php
+++ portal/ajax/connection.php
@@ -3,11 +3,10 @@
     date_default_timezone_set('Singapore');
     $servername = isset($_ENV['DB_HOST']) ? $_ENV['DB_HOST'] : "localhost";
     $username = isset($_ENV['DB_USER']) ? $_ENV['DB_USER'] : "udmlibra_udmad";
-    $pass = isset($_ENV['DB_PASS']) ? $_ENV['DB_PASS'] : 'Qp9(vICkO6C^';
+    $pass = isset($_ENV['DB_PASS']) ? $_ENV['DB_PASS'] : '%p6bIAgdd+u0';
     $dbname = isset($_ENV['DB_NAME']) ? $_ENV['DB_NAME'] : "udmlibra_udmdb";
     $conn = mysqli_connect($servername, $username, $pass, $dbname);
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-?>
